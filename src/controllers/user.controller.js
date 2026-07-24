e66b77e632bebceed64a05e0b539debcf1a2a673
Abu Rayhan Kobir <aburayhankobir013@gmail.com>
2026-07-25 02:47:38 +0600

    Add register controller and fixe some bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,65 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import ApiResponse from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (request, response) => {
-  return response.status(201).json({
-    message: "ok",
+  const { username, fullName, email, password } = request.body;
+  if (
+    [username, fullName, email, password].some((field) => {
+      return field?.trim() === "";
+    })
+  ) {
+    throw new ApiError(400, "All fields are required!");
+  }
+  const existedUser = await User.findOne({
+    $or: [
+      {
+        username,
+      },
+      {
+        email,
+      },
+    ],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exist!");
+  }
+  const avatarLocalPath = request.files?.avatar[0]?.path;
+  const coverImageLocalPath = request.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required!");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required!");
+  }
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    fullName,
+    email,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    coverImage,
+  });
+
+  const createdUser = await User.findById(uer._id).select(
+    "-password -refreshToken",
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user!");
+  }
+  return response.status(201).json(
+    new ApiResponse(200, createdUser, "User successfully registered!"),
+  );
 });
 
 const loginUser = asyncHandler(async (request, response) => {
@@ -12,7 +68,4 @@ const loginUser = asyncHandler(async (request, response) => {
   });
 });
 
-export {
-  registerUser,
-  loginUser,
-}
+export { registerUser, loginUser };
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -12,6 +12,4 @@ class ApiResponse {
   }
 }
 
-export default {
-  ApiResponse,
-}
+export default ApiResponse;
